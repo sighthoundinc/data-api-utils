989178a0c2054bbeac004d1ed8fb5089615fe81d
ericpsimon <ericpsimon@gmail.com>
2021-12-05 20:21:11 -0700

    Add more comments

--- src/client.py
+++ src/client.py
@@ -12,12 +12,14 @@ class DataApiClient:
         self.headers = {'Content-type': 'application/json', 'X-API-Key': f'{self.api_key}'}
 
     def query_stream_flat(self, query: StreamQuery):
+        """http://docs.data-api.boulderai.com/#query-flattened-stream-data"""
         print(f'Query => {query.toJSON()}')
         r = requests.post(f'{self.api_base}data/stream/query', data=query.toJSON(), headers=self.headers)
         r.raise_for_status()
         return r.json()
 
     def query_media_data(self, query: MediaQuery):
+        """http://docs.data-api.boulderai.com/#query-media-data"""
         print(f'Query => {query.toJSON()}')
         r = requests.post(f'{self.api_base}media/query', data=query.toJSON(), headers=self.headers)
         r.raise_for_status()
